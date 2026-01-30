80122a7cf64c5fece03052145e4e72fef57c8e6f
jovanohks <jovanoh400@gmail.com>
2026-01-30 09:52:53 +0000

    a

--- CT08_04/project4_bookOS.py
+++ CT08_04/project4_bookOS.py
@@ -8,4 +8,10 @@ for a,b in s.items():
     print(f"{a}: ${b}")
 x=True
 while x==True:
-    
+    x=input("What do you want to buy?")
+    if x in s:
+        y=int(input("how many do you want to buy"))
+    elif not x in s:
+        print("not in bookshop")
+    elif x =="no more":
+        break
