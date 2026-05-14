15aa6acf52876e2e5c7af6b7ae44b5b772e6ff1e
jovanohks <jovanoh400@gmail.com>
2026-05-14 11:37:41 +0000

    Co-authored-by: trainer1-computhink <trainer1-computhink@users.noreply.github.com>

---
+++ CT08_15/lesson15.py
@@ -0,0 +1,6 @@
+class Animal:
+    def __init__(self,name,species):
+        self.name=name
+        self.species=species
+    def describe_self():
+        print(f"this is a {self.name}, the {self.species}")
